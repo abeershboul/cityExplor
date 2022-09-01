c0a64a36eb35ce6a478aaff3893ff5d23d6dea50
abeershboul <abeer.shnoul@yahoo.com>
2022-09-01 19:45:31 +0300

    1

--- src/componant/App.js
+++ src/componant/App.js
@@ -1,14 +1,37 @@
 import React from "react";
-import CityMap from "./cityMap";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+//import axios from 'axios;'
 class App extends React.Component{
+  // constructor(props){
+  //   super(props);
+  //   this.state({
+  //     potoArray :[]
+  //   })
+  // }
+  getpoto=(event)=>{
+    event.preventDefault();
+    const searchQuery=event.target.name.value
+  }
   
   render(){
     return(
 <>
-<h1>City explor</h1>
-<CityMap/>
+<h1>scerh a poto</h1>
+
+ <Form onSubmit={this.getpoto}>
+      <fieldset disabled>
+        <Form.Group className="mb-3" >
+          <Form.Label htmlFor="name">searh about</Form.Label>
+          <Form.Control id="name" placeholder="what you seach about" />
+        </Form.Group>
+        
+        <Button type="submit">Searh</Button>
+      </fieldset>
+    </Form> 
 
 
 
